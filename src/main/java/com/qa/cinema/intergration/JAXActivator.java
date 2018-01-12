3b172f22c593a46f4c24a4608a37ab38126c7467
VayneValerius <joshuakingswell@gmail.com>
2018-01-12 11:23:01 +0000

    dbImplement

--- src/main/java/com/qa/cinema/intergration/JAXActivator.java
+++
@@ -1,8 +0,0 @@
-package com.qa.cinema.intergration;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("rest")
-public class JAXActivator extends Application {
-}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into Movie (id, title, genre, agerating) values (1, 'Harry Potter', 'Adventure', '12');
+insert into Movie (id, title, genre, agerating) values (2, 'IT', 'Horror', '18');
+insert into Movie (id, title, genre, agerating) values (3, 'Robocop', 'Action', '15');
